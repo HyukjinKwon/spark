ab41b08715f57e865a56cbc3e555d3a6989d9022
Hyukjin Kwon <gurwls223@apache.org>
2020-07-13 19:13:39 +0900

    Changes in root test

--- dev/run-tests.py
+++ dev/run-tests.py
@@ -662,14 +662,15 @@ def main():
                 changed_files = identify_changed_files_from_git_commits(
                     os.environ["GITHUB_SHA"], target_ref=os.environ["GITHUB_PREV_SHA"])
             print("changed_files : %s" % changed_files)
+
             modules_to_test = determine_modules_to_test(
                 determine_modules_for_files(changed_files), deduplicated=False)
+            print("modules_to_test : %s" % modules_to_test)
 
             if modules.root not in modules_to_test:
                 # If root module does not exist, only test the intersected modules.
                 # If root module is found, just run the modules as specified initially.
                 test_modules = list(set(modules_to_test).intersection(test_modules))
-
             print("test_modules : %s" % test_modules)
 
         changed_modules = test_modules
