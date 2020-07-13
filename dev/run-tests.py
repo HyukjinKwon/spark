196edd4383f60db0b158ff957a5a1af354df68e7
HyukjinKwon <gurwls223@apache.org>
2020-07-13 12:23:11 +0900

    show diff

--- dev/run-tests.py
+++ dev/run-tests.py
@@ -83,9 +83,8 @@ def identify_changed_files_from_git_commits(patch_sha, target_branch=None, targe
         # Note that Github Actions would run the tests without syncing to the latest
         # master.
         raw_output = subprocess.check_output(
-            ['git', 'diff', '--no-commit-id', '--name-only', '-r', patch_sha],
+            ['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', patch_sha],
             universal_newlines=True)
-        print(raw_output)
     else:
         if target_branch is None and target_ref is None:
             raise AttributeError("must specify either target_branch or target_ref")
