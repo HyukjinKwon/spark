e99e8a4b5704f6162097fbea279504dae38a3851
HyukjinKwon <gurwls223@apache.org>
2020-08-17 17:14:34 +0900

    Use MultiReporter

--- R/pkg/tests/run-all.R
+++ R/pkg/tests/run-all.R
@@ -61,16 +61,17 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
     set.seed(42)
 
     # TODO (SPARK-30663) To be removed once testthat 1.x is removed from all builds
-    if (grepl("^1\\..*", packageVersion("testthat"))) {
+    if (packageVersion("testthat")$major <= 1) {
       # testthat 1.x
       test_runner <- testthat:::run_tests
       reporter <- "summary"
-
     } else {
       # testthat >= 2.0.0
-      options(testthat.output_file = "target/test-reports/test-results.xml")
       test_runner <- testthat:::test_package_dir
-      reporter <- "junit"
+      reporter <- MultiReporter$new(list(
+        SummaryReporter$new(),
+        JunitReporter$new(file = "target/test-reports/test-results.xml")
+      ))
     }
 
     test_runner("SparkR",
